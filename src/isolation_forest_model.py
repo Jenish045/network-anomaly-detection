fe81911e44a0be5e0a9f1afedbdc4de5378e16a8
Jenish Upadhyay <jenishupadhyay04@gmail.com>
2026-03-13 15:25:57 +0530

    Implement isolation forest anamoly detection model

---
+++ src/isolation_forest_model.py
@@ -0,0 +1,20 @@
+from sklearn.ensemble import IsolationForest
+import numpy as np
+
+def train_isolation_forest(x):
+    model = IsolationForest(
+        n_estimators=100,
+        contamination=0.1,
+        random_state=42
+    )
+
+    model.fit(x)
+    return model
+
+def detect_anamolies(model,x):
+    predictions = model.predict(x)
+    return predictions 
+
+def anamoly_scores(model,x):
+    scores = model.decision_function(x)
+    return scores
